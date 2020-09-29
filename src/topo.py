10acd054239c34e6498a606e9752efd458cafd54
jin <sherry110534@gmail.com>
2020-09-29 10:52:44 +0800

    modify topo host name

--- src/topo.py
+++ src/topo.py
@@ -9,14 +9,14 @@ class MininetTopo(Topo):
     def build(self):
         # Add hosts to a topology
         self.addHost("h1")
-        self.addHost("h3")
+        self.addHost("h2")
         # Add switchs to a topology
         self.addSwitch("s1")
         self.addSwitch("s2")
         # Add bidirectional links to a topology, and set bandwidth(Mbps)
         self.addLink("h1", "s1", bw=2)
         self.addLink("s1", "s2", bw=2)
-        self.addLink("s2", "h3", bw=2)
+        self.addLink("s2", "h2", bw=2)
  
 if __name__ == '__main__':
     setLogLevel('info')
@@ -30,9 +30,9 @@ if __name__ == '__main__':
     # Start a network
     net.start()
     # Use tcpdump to record packet in background
-    print("start to record trace in h3 and h4")
-    h3 = net.get("h3")
-    h3.cmd("tcpdump -w ./output/h3_output.pcap &")
+    print("start to record trace in h2")
+    h2 = net.get("h2")
+    h2.cmd("tcpdump -w ./h2_output.pcap &")
 
     CLI(net)
     net.stop()
